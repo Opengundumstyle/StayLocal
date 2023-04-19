8e198f3918197dd020a79a827f3fcbdf73200eb9
Zhile Lin <zhlin04@gmail.com>
2023-04-19 13:38:54 -0700

    nav button

--- staylocal/app/components/navbar/Search.tsx
+++ staylocal/app/components/navbar/Search.tsx
@@ -16,6 +16,9 @@ const Search = () => {
               <div className="text-sm font-semibold px-6 ">
                    Anywhere
               </div>
+              <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">
+                   Any Week 
+              </div>
 
         </div>
       
